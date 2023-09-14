a7657ab4bea53db852a0fc11bec94168fe304da9
AbhishekSdeshmukh <abhimukh98@gmail.com>
2023-09-14 17:45:59 +0530

    4th commit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -52,17 +52,17 @@ const Dashboard = () => {
                                 className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                 <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
                                 <div className="dropdown no-arrow">
-                                    <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                    <a className="dropdown-toggle" href="https://example.com" role="button" id="dropdownMenuLink"
                                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                                     </a>
                                     <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
                                         aria-labelledby="dropdownMenuLink">
                                         <div className="dropdown-header">Dropdown Header:</div>
-                                        <a className="dropdown-item" href="#">Action</a>
-                                        <a className="dropdown-item" href="#">Another action</a>
+                                        <a className="dropdown-item" href="https://example.com">Action</a>
+                                        <a className="dropdown-item" href="https://example.com">Another action</a>
                                         <div className="dropdown-divider"></div>
-                                        <a className="dropdown-item" href="#">Something else here</a>
+                                        <a className="dropdown-item" href="https://example.com">Something else here</a>
                                     </div>
                                 </div>
                             </div>
@@ -83,17 +83,17 @@ const Dashboard = () => {
                                 className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                 <h6 className="m-0 font-weight-bold text-primary">Revenue Sources</h6>
                                 <div className="dropdown no-arrow">
-                                    <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                    <a className="dropdown-toggle" href="https://example.com" role="button" id="dropdownMenuLink"
                                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                                     </a>
                                     <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
                                         aria-labelledby="dropdownMenuLink">
                                         <div className="dropdown-header">Dropdown Header:</div>
-                                        <a className="dropdown-item" href="#">Action</a>
-                                        <a className="dropdown-item" href="#">Another action</a>
+                                        <a className="dropdown-item" href="https://example.com">Action</a>
+                                        <a className="dropdown-item" href="https://example.com">Another action</a>
                                         <div className="dropdown-divider"></div>
-                                        <a className="dropdown-item" href="#">Something else here</a>
+                                        <a className="dropdown-item" href="https://example.com">Something else here</a>
                                     </div>
                                 </div>
                             </div>
@@ -149,10 +149,10 @@ const Dashboard = () => {
                                         src="img/undraw_posting_photo.svg" alt="..."/>
                                 </div>
                                 <p>Add some quality, svg illustrations to your project courtesy of <a
-                                        target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
+                                        target="_blank" rel="noreferrer" href="https://undraw.co/">unDraw</a>, a
                                     constantly updated collection of beautiful svg images that you can use
                                     completely free and without attribution!</p>
-                                <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
+                                <a target="_blank" rel="noreferrer" href="https://undraw.co/">Browse Illustrations on
                                     unDraw &rarr;</a>
                             </div>
                         </div>
